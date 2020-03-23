77032cccf41ced0f95a9dae662d451a4736a28c5
joanaportugal <46449775+joanaportugal@users.noreply.github.com>
2020-03-23 12:16:29 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const port = 3002;
+
+const genRandomBool = () => Boolean(Math.random() >= 0.5);
+
+app.get("/", (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  return res.send(genRandomBool());
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
